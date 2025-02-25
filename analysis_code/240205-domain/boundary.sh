12efc77f1f9a2bb97524a9645a0198d116129b6d
rachelzeng98 <qiuruizeng98@gmail.com>
2025-02-25 00:18:09 +0000

    3c analysis

---
+++ analysis_code/240205-domain/boundary.sh
@@ -0,0 +1,53 @@
+hicluster domain --cell_table_path bulk_table/Astro-NT_NN.tsv --output_prefix hicluster_bulk_domain/Astro-NT_NN --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/Astro-TE_NN.tsv --output_prefix hicluster_bulk_domain/Astro-TE_NN --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/CA1-ProS_Glut.tsv --output_prefix hicluster_bulk_domain/CA1-ProS_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/CA3_Glut.tsv --output_prefix hicluster_bulk_domain/CA3_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/CEA-BST_Gaba.tsv --output_prefix hicluster_bulk_domain/CEA-BST_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/CS-PRNr-DR_En1_Sox2_Gaba.tsv --output_prefix hicluster_bulk_domain/CS-PRNr-DR_En1_Sox2_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/DG-PIR_Ex_IMN.tsv --output_prefix hicluster_bulk_domain/DG-PIR_Ex_IMN --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/DG_Glut.tsv --output_prefix hicluster_bulk_domain/DG_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/L23_IT_CTX_Glut.tsv --output_prefix hicluster_bulk_domain/L23_IT_CTX_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/L23_IT_ENT_Glut.tsv --output_prefix hicluster_bulk_domain/L23_IT_ENT_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/L23_IT_PPP_Glut.tsv --output_prefix hicluster_bulk_domain/L23_IT_PPP_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/L45_IT_CTX_Glut.tsv --output_prefix hicluster_bulk_domain/L45_IT_CTX_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/L5_ET_CTX_Glut.tsv --output_prefix hicluster_bulk_domain/L5_ET_CTX_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/L5_IT_CTX_Glut.tsv --output_prefix hicluster_bulk_domain/L5_IT_CTX_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/L5_NP_CTX_Glut.tsv --output_prefix hicluster_bulk_domain/L5_NP_CTX_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/L6_CT_CTX_Glut.tsv --output_prefix hicluster_bulk_domain/L6_CT_CTX_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/L6_IT_CTX_Glut.tsv --output_prefix hicluster_bulk_domain/L6_IT_CTX_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/L6bCT_ENT_Glut.tsv --output_prefix hicluster_bulk_domain/L6bCT_ENT_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/LA-BLA-BMA-PA_Glut.tsv --output_prefix hicluster_bulk_domain/LA-BLA-BMA-PA_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/LDT-PCG-CS_Gata3_Lhx1_Gaba.tsv --output_prefix hicluster_bulk_domain/LDT-PCG-CS_Gata3_Lhx1_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/Lamp5_Gaba.tsv --output_prefix hicluster_bulk_domain/Lamp5_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/MEA-BST_Gaba.tsv --output_prefix hicluster_bulk_domain/MEA-BST_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/MEA-COA_Glut.tsv --output_prefix hicluster_bulk_domain/MEA-COA_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/MRN-VTN-PPN_Pax5_Cdh23_Gaba.tsv --output_prefix hicluster_bulk_domain/MRN-VTN-PPN_Pax5_Cdh23_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/Microglia_NN.tsv --output_prefix hicluster_bulk_domain/Microglia_NN --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/OPC_NN.tsv --output_prefix hicluster_bulk_domain/OPC_NN --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/Oligo_NN.tsv --output_prefix hicluster_bulk_domain/Oligo_NN --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/PAG-PPN_Pax5_Sox21_Gaba.tsv --output_prefix hicluster_bulk_domain/PAG-PPN_Pax5_Sox21_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/PAG_Glut.tsv --output_prefix hicluster_bulk_domain/PAG_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/PB_Evx2_Glut.tsv --output_prefix hicluster_bulk_domain/PB_Evx2_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/Pvalb_Gaba.tsv --output_prefix hicluster_bulk_domain/Pvalb_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/STR-PAL_Chst9_Gaba.tsv --output_prefix hicluster_bulk_domain/STR-PAL_Chst9_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/STR_D1_Gaba.tsv --output_prefix hicluster_bulk_domain/STR_D1_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/STR_D1_Sema5a_Gaba.tsv --output_prefix hicluster_bulk_domain/STR_D1_Sema5a_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/STR_D2_Gaba.tsv --output_prefix hicluster_bulk_domain/STR_D2_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/STR_Gaba.tsv --output_prefix hicluster_bulk_domain/STR_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/SUB-ProS_Glut.tsv --output_prefix hicluster_bulk_domain/SUB-ProS_Glut --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/Sst_Gaba.tsv --output_prefix hicluster_bulk_domain/Sst_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
+hicluster domain --cell_table_path bulk_table/VLMC_NN.tsv --output_prefix hicluster_bulk_domain/VLMC_NN --resolution 25000 --save_count --window_size 10 --cpu 15 &
+wait
+hicluster domain --cell_table_path bulk_table/Vip_Gaba.tsv --output_prefix hicluster_bulk_domain/Vip_Gaba --resolution 25000 --save_count --window_size 10 --cpu 15 &
---
+++ analysis_code/240205-domain/merge.sh
@@ -0,0 +1,4 @@
+hicluster merge-cool --input_cool_tsv_file cell_table_for_merge/Oligo_NN.8wk.tsv --output_cool CellType.Age.Q.25kb/Oligo_NN.8wk.cool &
+hicluster merge-cool --input_cool_tsv_file cell_table_for_merge/Oligo_NN.18mo.tsv --output_cool CellType.Age.Q.25kb/Oligo_NN.18mo.cool &
+hicluster merge-cool --input_cool_tsv_file cell_table_for_merge/Oligo_NN.9mo.tsv --output_cool CellType.Age.Q.25kb/Oligo_NN.9mo.cool &
+wait
